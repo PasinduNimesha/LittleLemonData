ba08f4571b6596297a87a8c484db86ea25682f09
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-02-05 18:02:15 +0530

    minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.3")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -67,11 +67,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    kapt("androidx.room:room-compiler-processing:2.6.1")
-    implementation("androidx.room:room-runtime:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler-processing:2.4.3")
+    implementation("androidx.room:room-runtime:2.4.3")
+    annotationProcessor("androidx.room:room-compiler:2.4.3")
 // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-ktx:2.4.3")
 // To support LiveData in Compose
     implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
 }
--- app/src/main/java/com/littlelemon/littlelemondata/MainActivity.kt
+++ app/src/main/java/com/littlelemon/littlelemondata/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.littlelemon.littlelemondata
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionContext
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Delete
@@ -21,10 +33,14 @@ import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.Update
 import com.littlelemon.littlelemondata.ui.theme.LittleLemonDataTheme
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.runBlocking
 
 
 class MainActivity : ComponentActivity() {
+    private val inventoryRepository by lazy { InventoryRepository(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,26 +50,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val onUpdateItem: (Item) -> Unit = { item ->
+                        runBlocking(IO) {
+                            inventoryRepository.updateItem(item)
+                        }
+                    }
+                    val items = inventoryRepository.getAllItems().observeAsState(emptyList())
+                    InventoryList(items = items.value, onUpdateItem = onUpdateItem)
                 }
             }
         }
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "database-name"
-        ).build()
-        val playerDao = db.playerDao()
-        val players = playerDao.getAll()
+
     }
-    override fun onStart() {
-        super.onStart()
-        val sharedPreferences = getSharedPreferences("Little Lemon", MODE_PRIVATE)
-        val lastCount = sharedPreferences.getInt("StartCount", 0)
-        val newCount = lastCount + 1
-        Log.d("Count", newCount.toString())
-        sharedPreferences.edit().putInt("StartCount", newCount).apply()
-        Toast.makeText(this, "You have opened this app $newCount times", Toast.LENGTH_SHORT).show()
-    }
+//    override fun onStart() {
+//        super.onStart()
+//        val sharedPreferences = getSharedPreferences("Little Lemon", MODE_PRIVATE)
+//        val lastCount = sharedPreferences.getInt("StartCount", 0)
+//        val newCount = lastCount + 1
+//        Log.d("Count", newCount.toString())
+//        sharedPreferences.edit().putInt("StartCount", newCount).apply()
+//        Toast.makeText(this, "You have opened this app $newCount times", Toast.LENGTH_SHORT).show()
+//    }
 }
 
 @Composable
@@ -71,28 +88,72 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
-
-@Entity
-data class Player(
-    @PrimaryKey val id: Int,
-    val name: String,
-    val age: Int,
-)
-@Dao
-interface PlayerDao {
-    @Query("SELECT * FROM player")
-    fun getAll(): List<Player>
-
-    @Insert
-    fun insertAll(vararg players: Player)
-
-    @Delete
-    fun delete(player: Player)
+@Composable
+fun InventoryList(items: List<Item>, onUpdateItem: (item: Item) -> Unit) {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        LazyColumn{
+            itemsIndexed(items) { _, item ->
+                InventoryItem(item, onUpdateItem)
+            }
+        }
+    }
 }
 
-@Database(entities = [Player::class], version = 1)
+@Composable
+fun InventoryItem(item: Item, onUpdateItem: (item: Item) -> Unit) {
+    Row (
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = item.name)
+        TextButton(onClick = {
+            onUpdateItem(item.copy(quantity = item.quantity + 1))
+        }) {
+            Text(text = "+")
+        }
+        Text(text = item.quantity.toString())
+        TextButton(onClick = {
+            onUpdateItem(item.copy(quantity = item.quantity - 1))
+        }) {
+            Text(text = "-")
+        }
+    }
+}
+
+
+@Entity
+data class Item(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val quantity: Int,
+)
+@Dao
+interface InventoryDao {
+    @Query("SELECT * FROM Item order by name")
+    fun getAllItems(): LiveData<List<Item>>
+
+    @Update
+    fun updateItem(item: Item)
+}
+
+@Database(entities = [Item::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun playerDao(): PlayerDao
+    abstract fun inventoryDao(): InventoryDao
+}
+
+class InventoryRepository(context: Context){
+    private val database = Room.databaseBuilder(
+        context.applicationContext,
+        AppDatabase::class.java, "inventory.db"
+    ).createFromAsset("database/inventory.db").build()
+
+    fun getAllItems() = database.inventoryDao().getAllItems()
+
+    fun updateItem(item: Item) = database.inventoryDao().updateItem(item)
+
 }
 
 
